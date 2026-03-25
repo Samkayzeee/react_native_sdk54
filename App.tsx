01187e75b4e4d1436073982bc3f2539afc0f6696
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-03-25 21:00:23 +0100

    loaded the app on android app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>This is samkayzee</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  heading: {
+    color: 'green',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
