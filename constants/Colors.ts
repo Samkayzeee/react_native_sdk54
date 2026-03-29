eb88568700db5ddf2659408419ada9802919d1cb
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-03-29 16:07:11 +0100

    added the constant dir for the properties to be used

---
+++ constants/Colors.ts
@@ -0,0 +1,23 @@
+export const Colors = {
+    primary: "#3498db",
+    warning: "#e74c3c",
+
+    dark: {
+        text: "#d4d4d4",
+        background: "#333",
+        title: "#fff",
+        navBackground: "#444",
+        iconColor: "#9591a5",
+        iconColorFocused: "#fff",
+        uiBackground: "#2f2b3d",
+    },
+    light: {
+        text: "#333",
+        background: "#fff",
+        title: "#000",
+        navBackground: "#f0f0f0",
+        iconColor: "#333",
+        iconColorFocused: "#3498db",
+        uiBackground: "#f9f9f9",
+    }
+}
