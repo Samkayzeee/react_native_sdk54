4d1aea70a278c0af8a4051e92422d57a7ca411aa
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-03-29 15:46:25 +0100

    understanding the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <View style={{ flex: 1 }}>
-        <Stack />
-    </View>
-  )
-}
+    <Stack
+    screenOptions={{
+        headerStyle: { backgroundColor: "lightgray" },
+        headerTitleStyle: { color: "black", fontWeight: "bold" },
+        headerTintColor: "#333", // Back button color
+    }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" } } />
+      <Stack.Screen name="about" options={{ title: "About" } } />
+      <Stack.Screen name="contact" options={{ title: "Contact" } } />
+
+    </Stack>
+  );
+};
 
 export default RootLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
