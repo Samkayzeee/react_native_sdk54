24c39dec3a7a301c025f77c0ef3296b64e552727
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-03-30 18:27:53 +0100

    making use of components in the app

---
+++ components/ThemedView.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, useColorScheme, View, ViewProps } from 'react-native';
+import React from 'react';
+import { Colors } from '../constants/Colors';
+
+type ThemedViewProps = ViewProps;
+
+const ThemedView: React.FC<ThemedViewProps> = ({ style, ...props }) => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme || 'light']; // Fallback to light theme if colorScheme is null
+
+  return (
+    <View style={[{ backgroundColor: theme.background }, style]} 
+    {...props} 
+    />
+
+  );
+};
+
+export default ThemedView;
+
+const styles = StyleSheet.create({});
